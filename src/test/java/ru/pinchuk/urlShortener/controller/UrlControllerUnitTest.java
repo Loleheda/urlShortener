7167236e0c4b8a3a07f8456b1dfc32b8c3fb84d7
Loleheda <loleheda@gmail.com>
2024-05-07 18:33:24 +0300

    add readme

--- src/test/java/ru/pinchuk/urlShortener/controller/UrlControllerUnitTest.java
+++ src/test/java/ru/pinchuk/urlShortener/controller/UrlControllerUnitTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class UrlControllerTest {
+class UrlControllerUnitTest {
 
     @Mock
     private UrlService urlService;
